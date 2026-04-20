4d18e7a4bea7cdc91e1bbe1a9af040d2964fbb1b
AriJayaT <ari.teguh@binus.ac.id>
2026-04-20 12:21:17 +0700

    add firebase authentication

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,19 @@ import {Link, useLocalSearchParams} from 'expo-router';
 import { useState } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+import '../../firebase';
+
+const {firebaseConfig} = require('../../firebase');
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default function HomeScreen() {
   
